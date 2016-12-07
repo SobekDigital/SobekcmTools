38dea25fbe46e4a6415c1be881063f1cda644fd0
Mark Sullivan <mark.v.sullivan@sobekdigital.com>
2016-12-07 07:50:50 -0500

    Looks like some removes did not occur in the last commit

--- SobekCM_Rest_API/iPostSerialization.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SobekCM.Rest_API
-{
-    /// <summary> Interface is implemented for classes that require some additional configuration to occur around serialization events </summary>
-    public interface iSerializationEvents
-    {
-        /// <summary> Class is called by the serializer after an item is unserialized </summary>
-        void PostUnSerialization();
-    }
-}
